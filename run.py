03e26069830305afc1790b8b710200fb54c6fa73
Yahya Shahzad <yahyashahzadanees@gmail.com>
2026-01-01 04:17:06 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,3 @@
-import os
+from app import create_app
 
-def create_app():
-    app = Flask(__name__)
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///local.db"
-    )
-
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
-
-    return app
+app = create_app()
